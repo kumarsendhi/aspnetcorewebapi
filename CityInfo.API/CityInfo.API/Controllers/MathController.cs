133c044fcd1b0d16d37c62d2baa7c26e604535db
Kumar <kumar.sendhi@gmail.com>
2018-04-27 08:11:43 +0000

    status code commit

---
+++ CityInfo.API/CityInfo.API/Controllers/MathController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API.Controllers
+{
+    [Route("api/Math")]
+    public class MathController : Controller
+    {
+    }
+}
